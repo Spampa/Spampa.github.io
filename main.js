411127a6c45deae73fdabd840af4a7347b3d8c3c
Nicolò Spampatti <93142716+Spampa@users.noreply.github.com>
2022-08-19 22:49:16 +0200

    added skill container
    all container is workin progress...

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.full-menu');
 const container = document.querySelector('.container');
+const hero = document.querySelector('.hero');
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
         menu.classList.add('open');
         container.classList.add('hide');
+        hero.classList.add('hide');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         menu.classList.remove('open');
         container.classList.remove('hide');
+        hero.classList.remove('hide');
         menuOpen = false;
     }
 } );
