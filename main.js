b7b8dac57dd626d7ca8fe2c65d32711dd04ff3f9
Nicolò Spampatti <93142716+Spampa@users.noreply.github.com>
2022-08-20 15:10:25 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.full-menu');
-const container = document.querySelector('.container');
-const hero = document.querySelector('.hero');
+const body = document.querySelector('body');
+
 
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
         menu.classList.add('open');
-        container.classList.add('hide');
-        hero.classList.add('hide');
+        body.classList.add('notScroll');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         menu.classList.remove('open');
-        container.classList.remove('hide');
-        hero.classList.remove('hide');
+        body.classList.remove('notScroll');
         menuOpen = false;
     }
 } );
+
+menu.addEventListener('click', () => {
+    menuBtn.classList.remove('open');
+    menu.classList.remove('open');
+    body.classList.remove('notScroll');
+    menuOpen = false;
+} );
+
+
